f88796e872c7b6d9c56fdeb70145c0245fdd46ab
KDLAKSHAN <2019t00390@stu.cmb.ac.lk>
2022-10-15 08:18:28 +0530

    category change

--- app/src/main/java/com/example/vegiart365/MainActivity.java
+++ app/src/main/java/com/example/vegiart365/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
        Button Addcard =(Button) findViewById(R.id.Addcard);
        Button account=(Button) findViewById(R.id.account);
+       ImageView category=(ImageView) findViewById(R.id.category);
 
         Addcard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +35,23 @@ public class MainActivity extends AppCompatActivity {
              }
          });
 
+         category.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 opencategory();
+             }
+         });
+
     }
     public void openLogin(){
         Intent intent=new Intent(this,Login.class);
         startActivities(new Intent[]{intent});
     }
+
+    public void opencategory(){
+
+        Intent intent=new Intent(this,category.class);
+        startActivities(new Intent[]{intent});
+    }
+
 }
