5814ae6402f9b0475d753ead19c92b4519792c07
KDLAKSHAN <2019t00390@stu.cmb.ac.lk>
2022-10-15 13:27:49 +0530

    redister to login

--- app/src/main/java/com/example/vegiart365/RegistationActivity.java
+++ app/src/main/java/com/example/vegiart365/RegistationActivity.java
@@ -2,14 +2,31 @@ package com.example.vegiart365;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class RegistationActivity extends AppCompatActivity {
+    TextView Signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registation);
+
+        Signin = findViewById(R.id.Sign_in);
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistationActivity.this, Login.class));
+            }
+        });
+
     }
+
+
+
+
 }
